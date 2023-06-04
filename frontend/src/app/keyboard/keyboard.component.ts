6646db4362986ff444b5b305b498b4e2e0ea0403
ryan <ryanmegarity@gmail.com>
2023-06-04 11:26:55 +0100

    amend event binding

--- frontend/src/app/keyboard/keyboard.component.ts
+++ frontend/src/app/keyboard/keyboard.component.ts
@@ -1,34 +1,30 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-keyboard',
   templateUrl: './keyboard.component.html',
   styleUrls: ['./keyboard.component.css'],
 })
-export class KeyboardComponent implements AfterViewInit {
-
+export class KeyboardComponent {
   private context: CanvasRenderingContext2D = {} as CanvasRenderingContext2D;
 
   topKeys: string[] = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
   middleKeys: string[] = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
   bottomKeys: string[] = ['Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.'];
 
-  ngAfterViewInit(): void {
-    this.addKeyListeners();
+
+  @HostListener('document:keydown', ['$event'])
+  onKeydown(event: KeyboardEvent): void {
+    const keyToStyle = document.getElementById(
+      `${event.key.toLocaleUpperCase()}-key-card`
+    );
+    if (!keyToStyle) {
+      return;
+    }
+    keyToStyle.classList.add('key-pressed');
+    setTimeout(() => {
+      keyToStyle.classList.remove('key-pressed');
+    }, 200);
   }
 
-  addKeyListeners(): void {
-    document.onkeydown = (event: KeyboardEvent) => {
-      const keyToStyle = document.getElementById(
-        `${event.key.toLocaleUpperCase()}-key-card`
-      );
-      if (!keyToStyle) {
-        return;
-      }
-      keyToStyle.classList.add('key-pressed');
-      setTimeout(() => {
-        keyToStyle.classList.remove('key-pressed');
-      }, 200);
-    };
-  }
 }
